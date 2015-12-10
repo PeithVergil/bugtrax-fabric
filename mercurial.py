c3807b131eb652a43e95588fee906c5d08be5d56
Peith Vergil <peith.vergil@gmail.com>
2015-12-10 14:46:22 +0800

    Install mercurial dependencies and create a new virtual environment.

--- mercurial.py
+++ mercurial.py
@@ -1,4 +1,4 @@
-from fabric.api import cd, run, task, prefix
+from fabric.api import cd, run, sudo, task, prefix
 
 import utils
 
@@ -12,9 +12,23 @@ def install():
 
     fab --config=config.conf mercurial.install
     """
-    venvs = utils.home('venvs')
+    # Python build headers.
+    packages = [
+        'python-setuptools',
+        'python-virtualenv',
+        'python-dev',
+        'python-tk',
+    ]
+
+    # Install python build headers.
+    sudo('apt-get -y install {}'.format(' '.join(packages)))
+
+    # Put all virtual environments in one directory.
+    run('mkdir -p {}'.format(utils.home('venvs')))
 
     # Create the virtual environment.
+    venvs = utils.home('venvs')
+
     with cd(venvs):
         run('virtualenv mercurial')
 
